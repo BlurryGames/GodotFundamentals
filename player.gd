42897e8b0d24d15f8f759b4748106b7a1a1f1d39
BlurryGames <julian.lb.games@gmail.com>
2026-04-28 17:09:45 +0200

    Section1: Get Started Project / Lesson6: Firing Bullets

--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
 class_name Player extends Node2D
 
+@export var bullet_scene: PackedScene = null
+
 func _ready() -> void:
 	print("Hello World!")
 
@@ -9,4 +11,7 @@ func _process(_delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_pressed():
-			print("Shoot!")
+			var bullet: RigidBody2D = bullet_scene.instantiate()
+			add_child(bullet)
+			bullet.set_global_transform(get_global_transform())
+			bullet.apply_central_impulse(get_global_transform().x * 1_000.0)
